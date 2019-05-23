d9de31573e357b8dda3faeae9ba10e7a0b210352
Laura Bodell <bodelllaura@gmail.com>
2019-05-23 15:38:15 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,6 +1,7 @@
  
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Todo from './Todo'
  
 class TodosContainer extends Component {
   
