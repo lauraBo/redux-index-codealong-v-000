2c37c5db7c209059391083157fe7bc073e6e3dbe
Laura Bodell <bodelllaura@gmail.com>
2019-05-23 15:35:23 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
  
 class TodosContainer extends Component {
+  
+   renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
  
   render() {
     return(
